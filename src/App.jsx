27afb7b497036f297fc45674b1fd5e999751cdd4
Oyawe <oyawegoodness@gmail.com>
2023-08-30 18:19:35 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+// import UilReact from "@iconscout/react-unicons/icons/uil-react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mx-auto max-w-screen-md mt-4 py-5 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl, shadow-gray-700"></div>
+    </div>
+  );
+}
+
+export default App;
