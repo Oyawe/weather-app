9a1791a8c1943aa1c1985be70b3718dd0c798b72
Oyawe <oyawegoodness@gmail.com>
2023-08-30 18:42:21 -0700

    created dummy navLinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import TopButtons from "./components/TopButtons";
 // import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
   return (
     <div className="App">
-      <div className="mx-auto max-w-screen-md mt-4 py-5 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl, shadow-gray-700"></div>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <TopButtons />
+      </div>
     </div>
   );
 }
