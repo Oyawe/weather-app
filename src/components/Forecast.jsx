8eb81ac41d513def310f729a56615c736f5b6dcb
Oyawe <oyawegoodness@gmail.com>
2023-08-30 22:36:35 -0700

    fixed some bugs

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -4,13 +4,14 @@
 const Forecast = ({ title }) => {
   return (
     <div>
-      <div className="flex items-center justify-start mt-6 mx-12">
+      <div className="flex items-center justify-start mt-6 px-4">
         <p className="text-white font-medium uppercase">{title}</p>
       </div>
-      <hr className="my-2 mx-12" />
+      <hr className="my-2 mx-4" />
 
-      <div className="flex flex-row items-center justify-between text-white">
-        <div className="flex flex-col items-center justify-center mx-12">
+      {/* <div className="flex flex-row items-center justify-between text-white"> */}
+      <div className="flex flex-row items-center justify-between space-x-2 text-white py-2 px-2">
+        <div className="flex flex-col items-center justify-center">
           <p className="font-light text-sm">04:30 PM</p>
           <img
             src="https://freepngimg.com/thumb/sun/4-2-sun-png-picture-thumb.png"
